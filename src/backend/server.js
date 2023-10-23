0c8f185ea5778763ebdc91aa53b809fd4cf38203
Yamm Elnekave <klavlavigugu@gmail.com>
2023-10-22 19:24:43 -0700

    making server

---
+++ src/backend/server.js
@@ -0,0 +1,37 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const app = express();
+
+const gameRoutes = require('./routes/game_info');
+
+// express.json() is a middleware that parses json data
+app.use(express.json());
+
+// middleware, this will run before any get request
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+})
+
+// request is what we get from the client
+// response is what we send to the client
+app.get('/', (req, res) => {
+    res.json({mssg: 'Hello World'});
+
+    // figure out if signed in and send to
+    // /login
+
+
+    // or /game/GAMEID
+
+
+})
+
+app.use('/api/game_info', gameRoutes);
+
+
+app.listen(process.env.PORT, () => {
+    console.log('server is running on port', process.env.PORT); 
+});
