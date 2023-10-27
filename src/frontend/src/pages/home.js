2e6f4625fb1c2676f0d04543c25ceca8f5e7932b
Yamm Elnekave <klavlavigugu@gmail.com>
2023-10-26 22:04:56 -0700

    home page

--- src/frontend/src/pages/home.js
+++ src/frontend/src/pages/home.js
@@ -1,7 +1,38 @@
+import { useEffect, useState } from "react";
+
 function Home(){
+    const [games, setGames] = useState(null);
+
+    useEffect(() => {
+        const fetchGames = async () => {
+            const res = await fetch('/api/dummy'); 
+            
+            if (res.ok) {
+                const data = [];
+                data.push(
+                    await res.json()
+                );   
+                console.log("data: " + data); 
+                console.log("name: " + data.name)
+                setGames(data);
+            }
+            
+        }
+        fetchGames();
+    }, []); // empty array means only run once
+
+
     return (
         <div className="home">
             <h1>Home</h1>
+            <div className="Games">
+                {games && games.map((game) => (
+                    <div className="game" key={game._id}>
+                        <h2>{game.name}</h2>
+                        <p>{game.description}</p>
+                    </div>
+                ))}
+            </div>
         </div>
     );
 };
