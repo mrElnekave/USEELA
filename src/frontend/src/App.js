6c1264b909531f0daab794a8d21580de366acb6d
ZCX031116 <122865502+ZCX031116@users.noreply.github.com>
2023-11-14 18:41:32 -0800

    Revert "Image data extraction"

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -4,32 +4,15 @@ import Home from './pages/home';
 import Map from './pages/map';
 import Dummy from './pages/dummy';
 import Navbar from './components/Navbar';
-import ImageUpload from './pages/ImageUpload';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <div className="App">
-          <Navbar></Navbar>
-          <h1>Main Page</h1>
-        </div>
-        
-        <Routes>
-          <Route path='/' element={<Home />}>
-
-          </Route>
-          <Route path='/dummy' element={<Dummy />}>
-
-          </Route>
-          <Route path='/ImageUpload' element={<ImageUpload />}>
-        
-          </Route>
-        </Routes>
-        
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Main Page</h1>
+    </div>
   );
 }
 
