d2e560b2e5dc1931132315ea4622b48761b5f54b
ZCX031116 <Cixuan_Zhang@outlook.com>
2023-11-15 20:50:02 -0800

    merge frontend and ImageUpload
    upload App.js, index.js, Navbar.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -5,31 +5,15 @@ import Map from './pages/map';
 import Dummy from './pages/dummy';
 import Navbar from './components/Navbar';
 import ImageUpload from './pages/ImageUpload';
+
 import './App.css';
 
 function App() {
   return (
-    <>
-      <BrowserRouter>
-        <div className="App">
-          <Navbar></Navbar>
-          <h1>Main Page</h1>
-        </div>
-        
-        <Routes>
-          <Route path='/' element={<Home />}>
-
-          </Route>
-          <Route path='/dummy' element={<Dummy />}>
-
-          </Route>
-          <Route path='/ImageUpload' element={<ImageUpload />}>
-        
-          </Route>
-        </Routes>
-        
-      </BrowserRouter>
-    </>
+    <div className="App">
+      <Navbar></Navbar>
+      <h1>Main Page</h1>
+    </div>
   );
 }
 
