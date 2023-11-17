15f5d0fa2eebe5147ae7654824d22618ebfc9acf
ZCX031116 <Cixuan_Zhang@outlook.com>
2023-11-17 14:22:34 -0800

    move features of image uploading in server.js
    to controller.js and game_info.js

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,19 +2,12 @@ require('dotenv').config();
 
 const mongoose = require('mongoose');
 const express = require('express');
-const multer = require('multer');
-const ExifReader = require('exifreader');
+
 
 const app = express();
 
 const gameRoutes = require('./routes/game_info');
 const dummyRoutes = require('./routes/dummy');
-
-
-const Quiz = require('./models/Quiz'); // 
-
-
-const upload = multer({ storage: multer.memoryStorage() });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -30,55 +23,6 @@ app.get('/', (req, res) => {
 
 app.use('/api/game_info', gameRoutes);
 app.use('/api/dummy', dummyRoutes);
-
-
-function extractGPSData(exifData) {
-    if (!exifData) return null;
-
-    const latitude = exifData.GPSLatitude ? exifData.GPSLatitude.description : null;
-    const longitude = exifData.GPSLongitude ? exifData.GPSLongitude.description : null;
-    const altitude = exifData.GPSAltitude ? exifData.GPSAltitude.description : null;
-
-    return { latitude, longitude, altitude };
-}
-
-
-app.post('/upload', upload.array('photos'), async (req, res) => {
-    try {
-        console.log(req.files);
-        const { name, description } = req.body;
-        const actual_locations = [];
-        const imageBuffers = [];
-
-        for (const file of req.files) {
-            let exifData;
-            try {
-                exifData = ExifReader.load(file.buffer);
-            } catch (error) {
-                console.error('Error extracting EXIF data:', error);
-            }
-
-            const GpsData = extractGPSData(exifData);
-            actual_locations.push(GpsData);
-            console.log(GpsData);
-            imageBuffers.push(file.buffer);
-        }
-
-        const newQuiz = new Quiz({
-            name: name,
-            description: description,
-            images: imageBuffers, 
-            actual_locations: actual_locations
-        });
-
-        await newQuiz.save();
-        res.status(200).json({ message: "Quiz uploaded successfully", quiz: newQuiz });
-    } catch (error) {
-        console.error("Error uploading quiz:", error);
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-});
-
 
 app.listen(process.env.PORT, () => {
     console.log('Server is running on port', process.env.PORT);
