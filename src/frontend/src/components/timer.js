9f3716a17a0691cd2623b531488ea7937dc1c916
XiaohanSong0930 <songxiaohan0930@gmail.com>
2023-11-27 01:14:25 -0800

    Improved game.js.
    - Set pictures as background images;
    - add transparency and transform of the map;
    - rounds, scores, time left;

---
+++ src/frontend/src/components/timer.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect } from 'react';
+import { Typography } from '@mui/material';
+
+export default function Timer({ onTimeUp }) {
+  const [timeLeft, setTimeLeft] = useState(60);
+
+  useEffect(() => {
+    // 退出前清除定时器
+    const timer = timeLeft > 0 && setInterval(() => setTimeLeft(timeLeft - 1), 1000);
+    if (timeLeft === 0) {
+      onTimeUp();
+    }
+    return () => clearInterval(timer);
+  }, [timeLeft, onTimeUp]);
+
+  return (
+    <Typography variant="h6" sx={{ color: 'black' }}>
+      {`${Math.floor(timeLeft / 60).toString().padStart(2, '0')}:${(timeLeft % 60).toString().padStart(2, '0')}`}
+    </Typography>
+  );
+}
