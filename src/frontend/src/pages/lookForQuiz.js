3197b14f40818e4586ed1e68938edc27cf8c5938
Rakil Kim <rakilkim0216@gmail.com>
2023-11-27 02:32:50 -0800

    change website title, pretty up lookup page

--- src/frontend/src/pages/lookForQuiz.js
+++ src/frontend/src/pages/lookForQuiz.js
@@ -1,44 +1,51 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Box } from '@mui/material';
+import SearchResults from '../providers/SearchResults.js'
 
 const LookForQuiz = () => {
   const [searchTerm, setSearchTerm] = useState('');
-
+  const data = useNavigate()
+  const [gameImages, setGameImages] = useState(["testPics/pic1.jpeg"]); // an array with image paths
+  const [gameAnswers, setGameAnswers] = useState([{lat: 45.464664, lon: 9.188540}]);
+  const [names, setNames] = useState(['quiz name'])
   const handleSearch = () => {
     // Here's the function to tell backend the name to search and fetch names from backend
     // TODO: We need to know the backend function we should call
     console.log(`Searching for ${searchTerm}`);
+    const n = searchTerm;
+    setNames(n);
+    // TODO: need to set the name of the quizzes that we get from database
   };
 
   return (
-    <div style={styles.container}>
-      <input 
-        type="text" 
-        placeholder="Search for a quiz" 
+    <Box sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    }}>
+    <Box sx={{
+      display: 'flex',
+      justifyContent: 'center',
+      marginTop: 8,
+      marginBottom: 2,
+    }}>
+      <TextField
+        label="Search for a quiz" 
+        variant="outlined"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        style={styles.input}
       />
-      <button onClick={handleSearch} style={styles.button}>Search</button>
-    </div>
+      <Button variant="contained" onClick={handleSearch}
+      sx={{
+        mt: 1,
+        ml: 1,
+      }}
+      >Search</Button>
+    </Box>
+    <SearchResults results={names}/>
+    </Box>
   );
-};
-
-const styles = {
-  container: {
-    textAlign: 'center',
-    padding: '20px',
-  },
-  input: {
-    padding: '10px',
-    marginRight: '5px',
-  },
-  button: {
-    padding: '10px 20px',
-    backgroundColor: '#2774AE',
-    color: 'white',
-    border: 'none',
-    borderRadius: '5px',
-  }
 };
 
 export default LookForQuiz;
