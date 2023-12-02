26a765e862fab3a5d8f48adf68e1d6bfcfb1fefc
Rakil Kim <rakilkim0216@gmail.com>
2023-12-01 17:30:22 -0800

    Lobby and profile works now with actual data

--- src/frontend/src/pages/profile.js
+++ src/frontend/src/pages/profile.js
@@ -5,23 +5,23 @@ import { useParams } from 'react-router-dom';
 
 const Profile = () => {
   // Replace with actual data
-  const userIdForNow = '6566909e5b3dd9dbb06f7795';
   const [userData, setUserData] = useState(null);
   const userId = (useParams().userid);
   const fetchUser = async () => {
     // Call to getUser API
-    const response = await fetch(`/api/user_info/${userIdForNow}`);
+    const response = await fetch(`/api/user_info/${userId}`);
     if (!response.ok) {
         console.log("error fetching data");
         return;
     }
     const data = await response.json();
-    console.log(data);
     setUserData(data);
+    console.log(data)
 };
 useEffect(()=>{
   fetchUser();
-}, [userIdForNow]);
+}, [userId]);
+
 
   return (
     <Container sx={{
@@ -46,7 +46,7 @@ useEffect(()=>{
         mt: 2,
       }}><img src={image} alt='profile picture' style=
       {{ maxWidth: '200px', marginBottom: '15px', WebkitClipPath: 'circle(75px at 50% 50%)', clipPath: 'circle(75px at 50% 50%)', }} /></Box>
-      <Typography variant='h4' gutterBottom sx={{fontWeight: 'bold',}}>{userData.email}</Typography>
+      <Typography variant='h4' gutterBottom sx={{fontWeight: 'bold',}}>{userData.email.split("@")[0]}</Typography>
       <Typography variant='h6' gutterBottom>Rank: {userData.rank}</Typography>
       <Typography variant='body1' sx={{mb:5,}}>Contributed Quizzes: {userData.quizzes}</Typography>
     </Card>
