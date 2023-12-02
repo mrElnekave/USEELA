3dbc72224f72268b35c5ca0a2e1ddae04a3aa884
Yamm Elnekave <klavlavigugu@gmail.com>
2023-12-01 16:32:48 -0800

    got rid of all useless project files

--- start.sh
+++ start.sh
@@ -1,26 +1,5 @@
 npm install
 
-
-# # back
-# npm install express
-# sudo npm install -g nodemon # you might need to run this as sudo
-# npm install dotenv
-# npm install mongodb
-# npm install mongoose
-# npm install exifreader --save 
-# npm install multer
-# npm install sharp # convert pictures to .jpeg
-# npm install heic-convert # extra converter for .heic
-
-
-# # front
-# npm install react-router-dom
-# npm install react-dropzone
-# npm install rsuite
-# npm install @react-google-maps/api
-# npm install @mui/material
-# npm install @emotion/styled
-
-./back.sh & 
+nodemon ./src/backend/server.js & 
 sleep 2 # wait for backend to start
 ./front.sh
